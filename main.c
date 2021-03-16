228fa3606b030f06f6ca94422aa5cf67739d9524
Ilias Tafah <iltafah@e2r9p8.1337.ma>
2021-03-16 12:52:30 +0100

    I think I finished tokenazation

--- main.c
+++ main.c
@@ -6,18 +6,18 @@
 /*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 09:28:37 by iltafah           #+#    #+#             */
-/*   Updated: 2021/03/14 16:10:27 by iltafah          ###   ########.fr       */
+/*   Updated: 2021/03/16 12:43:14 by iltafah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "./minishell.h"
+#include "./minishell.h"
 
-void	print_current_dir()
+void print_current_dir()
 {
-	char	*curr_dir;
-	int		keep_index;
-	int		len;
-	int		i;
+	char *curr_dir;
+	int keep_index;
+	int len;
+	int i;
 
 	i = 0;
 	len = 0;
@@ -30,63 +30,62 @@ void	print_current_dir()
 		i++;
 	}
 	len = i - keep_index;
-	write(1, GRN, ft_strlen(YEL));
+	write(1, GRN, ft_strlen(GRN));
 	write(1, "➜    ", 5);
 	write(1, curr_dir + keep_index, len);
 	write(1, " ", 1);
 	free(curr_dir);
-	write(1, PRP, ft_strlen(RED));
+	write(1, PRP, ft_strlen(PRP));
 }
 
-void	print_header()
+void print_header()
 {
 	printf(CYN);
 	printf("╭━╮╭━┳━━┳━╮╱╭┳━━┳━━━┳╮╱╭┳━━━┳╮╱╱╭╮   \n");
-	printf("┃┃╰╯┃┣┫┣┫┃╰╮┃┣┫┣┫╭━╮┃┃╱┃┃╭━━┫┃╱╱┃┃   \n"); 
-	printf("┃╭╮╭╮┃┃┃┃╭╮╰╯┃┃┃┃╰━━┫╰━╯┃╰━━┫┃╱╱┃┃   \n"); 
-	printf("┃┃┃┃┃┃┃┃┃┃╰╮┃┃┃┃╰━━╮┃╭━╮┃╭━━┫┃╱╭┫┃╱╭╮\n"); 
-	printf("┃┃┃┃┃┣┫┣┫┃╱┃┃┣┫┣┫╰━╯┃┃╱┃┃╰━━┫╰━╯┃╰━╯┃\n"); 
+	printf("┃┃╰╯┃┣┫┣┫┃╰╮┃┣┫┣┫╭━╮┃┃╱┃┃╭━━┫┃╱╱┃┃   \n");
+	printf("┃╭╮╭╮┃┃┃┃╭╮╰╯┃┃┃┃╰━━┫╰━╯┃╰━━┫┃╱╱┃┃   \n");
+	printf("┃┃┃┃┃┃┃┃┃┃╰╮┃┃┃┃╰━━╮┃╭━╮┃╭━━┫┃╱╭┫┃╱╭╮\n");
+	printf("┃┃┃┃┃┣┫┣┫┃╱┃┃┣┫┣┫╰━╯┃┃╱┃┃╰━━┫╰━╯┃╰━╯┃\n");
 	printf("╰╯╰╯╰┻━━┻╯╱╰━┻━━┻━━━┻╯╱╰┻━━━┻━━━┻━━━╯\n");
 }
 
-t_node	*create_single_node()
+t_node *create_single_node()
 {
 	t_node *node = malloc(sizeof(t_node));
-	
+
 	node->bottom = NULL;
 	node->next = NULL;
-	
+
 	return (node);
 }
 
-t_node	*create_pipline()
+t_node *create_pipline()
 {
 	t_node *node = create_single_node();
 
 	node->tag = e_pipeline_node;
-	
 
 	return (node);
 }
 
-t_node	*create_word(char *cmd_part)
+t_node *create_word(char *cmd_part)
 {
 	t_node *node = create_single_node();
-	
+
 	node->tag = e_word_node;
 	node->u_infos.word = cmd_part;
 
 	return (node);
 }
 
-t_node	*create_simple_command(char *input_files, char *outputfiles)
+t_node *create_simple_command(char *input_files, char *outputfiles)
 {
 	t_node *node = create_single_node();
-	
+
 	node->tag = e_simple_cmd_node;
 	node->u_infos.redirections.input_files = input_files;
 	node->u_infos.redirections.output_files = outputfiles;
-	
+
 	return (node);
 }
 
@@ -95,44 +94,43 @@ t_node	*create_simple_command(char *input_files, char *outputfiles)
 // 	t_node_list *node_list = malloc(sizeof(t_node_list));
 // 	node_list->elem = create_single_node(lable);
 // 	node_list->next = NULL;
-// 	return (node_list);	
+// 	return (node_list);
 // }
 
-void	print_preorder(t_node *node)
+void print_preorder(t_node *node)
 {
 	if (node == NULL)
 	{
 		printf("\n");
-		return ;
+		return;
 	}
 	if (node->tag == e_pipeline_node)
-		printf("%s>>>>>> pipline <<<<<<\n",RED);
+		printf("%s>>>>>> pipline <<<<<<\n", RED);
 	else if (node->tag == e_word_node)
 		printf("%s%s ", YEL, node->u_infos.word);
 	else if (node->tag == e_simple_cmd_node)
-		printf("%s\n\n>>>simple command<<<\n",RED);
+		printf("%s\n\n>>>simple command<<<\n", RED);
 	print_preorder(node->bottom);
 	print_preorder(node->next);
 }
 
-void	print_postorder(t_node *node)
+void print_postorder(t_node *node)
 {
 	if (node == NULL)
-		return ;
+		return;
 	print_postorder(node->bottom);
 	print_postorder(node->next);
 	printf("%s, ", node->u_infos.word);
 }
 
-void	print_inorder(t_node *node)
+void print_inorder(t_node *node)
 {
 	if (node == NULL)
-		return ;
+		return;
 	print_inorder(node->bottom);
 	printf("%s, ", node->u_infos.word);
 	print_inorder(node->next);
 }
-
 
 // void	fill_tree(char *line, t_node **mother_node)
 // {
@@ -143,10 +141,10 @@ void	print_inorder(t_node *node)
 // 	///supouse that the line doesn't have any errors
 // 		curr_cmd_node = (*mother_node)->bottom;
 // 		curr_cmd_node = create_simple_command(NULL, NULL);
-		
+
 // 		////////
 // 		curr_word_node = curr_cmd_node->bottom;
-		
+
 // 		curr_word_node = create_word("ls");
 // 		curr_word_node = curr_word_node->next;
 // 		curr_word_node = create_word("-l");
@@ -154,26 +152,25 @@ void	print_inorder(t_node *node)
 // 		curr_word_node = create_word("-a");
 // 		////////
 
-
 // 		curr_cmd_node = curr_cmd_node->next;
 // 		curr_cmd_node = create_simple_command(NULL, NULL);
-	
+
 // 		////////
 // 		curr_word_node = curr_cmd_node->bottom;
-		
+
 // 		curr_word_node = create_word("sleep");
 // 		curr_word_node = curr_word_node->next;
 // 		curr_word_node = create_word("10");
 // 		curr_word_node = curr_word_node->next;
 // 		curr_word_node = create_word("20");
 // 		////////
-		
+
 // 		curr_cmd_node = curr_cmd_node->next;
 // 		curr_cmd_node = create_simple_command(NULL, NULL);
-	
+
 // 		////////
 // 		curr_word_node = curr_cmd_node->bottom;
-		
+
 // 		curr_word_node = create_word("sleep");
 // 		curr_word_node = curr_word_node->next;
 // 		curr_word_node = create_word("10");
@@ -182,44 +179,40 @@ void	print_inorder(t_node *node)
 // 		////////
 // }
 
-void	fill_cmd_node(t_node **curr_cmd_node)
+void fill_cmd_node(t_node **curr_cmd_node)
 {
 	*curr_cmd_node = create_simple_command(NULL, NULL);
 }
 
-void	fill_word_node(t_node **curr_word_node, char *word)
+void fill_word_node(t_node **curr_word_node, char *word)
 {
 	*curr_word_node = create_word(word);
 }
 
-
-
 //////////////////////////////////////////////
-t_node	**get_next_node(t_node **curr_node)
+t_node **get_next_node(t_node **curr_node)
 {
 	return (&((*curr_node)->next));
 }
 
-t_node	**get_bottom_node(t_node **curr_node)
+t_node **get_bottom_node(t_node **curr_node)
 {
 	return (&((*curr_node)->bottom));
 }
 //////////////////////////////////////////////
 
-
-
-t_node	**get_node(t_node **curr_node, int dir)
+t_node **get_node(t_node **curr_node, int dir)
 {
 	if (dir == BOTTOM)
 		return (&((*curr_node)->bottom));
 	return (&((*curr_node)->next));
 }
 
-char	*get_token_dnt_need_this(char **line)
+char *get_token_dnt_need_this(char **line)
 {
-	int		i;
-	int		len;
-	char	*ptr;
+	int i;
+	int len;
+	char *ptr;
 
 	i = 0;
 	len = 0;
@@ -255,25 +248,24 @@ char	*get_token_dnt_need_this(char **line)
 	return (*line);
 }
 
-void	parse_line(char **line, t_node **mother_node)
+void parse_line(char **line, t_node **mother_node)
 {
-	t_node	**curr_cmd_node;
-	t_node	**curr_word_node;
-	char	*token;
-	int		new_cmd_node;
-	
+	t_node **curr_cmd_node;
+	t_node **curr_word_node;
+	char *token;
+	int new_cmd_node;
+
 	*mother_node = create_pipline();
 	curr_cmd_node = mother_node;
 
 	curr_cmd_node = get_node(curr_cmd_node, BOTTOM);
 	fill_cmd_node(curr_cmd_node);
-	
+
 	token = get_token_dnt_need_this(line);
 	curr_word_node = get_node(curr_cmd_node, BOTTOM);
 	fill_word_node(curr_word_node, token);
 
-
-	printf("%s\nTokens given from the cmd line : \n\n|%s|    \n",WHT,token);
+	printf("%s\nTokens given from the cmd line : \n\n|%s|    \n", WHT, token);
 	while ((*line)[0] != '\0')
 	{
 		token = get_token_dnt_need_this(line);
@@ -296,12 +288,11 @@ void	parse_line(char **line, t_node **mother_node)
 		}
 		printf("|%s|    \n", token);
 	}
-
 }
 ///////////////////////////////////////////////////////////////////////////////////
-t_tokens	*create_single_token(char *data, t_type type)
+t_tokens *create_single_token(char *data, t_type type)
 {
-	t_tokens	*token_node;
+	t_tokens *token_node;
 
 	token_node = malloc(sizeof(t_tokens));
 	token_node->data = data;
@@ -310,48 +301,97 @@ t_tokens	*create_single_token(char *data, t_type type)
 	return (token_node);
 }
 
-char	*treat_double_quotes(char **line)
+char *treat_double_quotes(char **line)
 {
-	char		*token;
-	int			i;
-
-	i = 0;
-	(*line)++;
-	while ((*line)[i] != '"' && (*line)[i] != '\0')
-		i++;
-	token = ft_substr(*line, 0, i);
-	*line = *line + i;
-	return (token);
-}
-
-char	*treat_simple_word(char **line)
-{
-	char		*token;
-	int			i;
-	
-	i = 0;
-	while ((*line)[i] != ' ' && (*line)[i] != '\''
-	&& (*line)[i] != '"' && (*line)[i] != '\0')
-		i++;
-	token = ft_substr(*line, 0, i);
-	*line = *line + i;
-	return (token);
-}
-
-char	*get_token(char **line)
-{
+	int		i;
+	int		closed;
 	char	*token;
+
+	i = 1;
+	closed = 0;
+	while (((*line)[i] != ' ' || closed == 0) && (*line)[i] != '|' && (*line)[i] != ';' &&
+	(*line)[i] != '>' && (*line)[i] != '<'  && (*line)[i] != '\0')
+		i++;
+	token = ft_substr(*line, 0, i);
+	*line = *line + i;
+	return (token);
+}
+
+char *treat_single_quotes(char **line)
+{
+	int		i;
+	int		closed;
+	char	*token;
+
+	i = 0;
+	closed = 0;
+	while (((*line)[i] != ' ' || closed == 0) && (*line)[i] != '|' && (*line)[i] != ';' &&
+	(*line)[i] != '>' && (*line)[i] != '<'  && (*line)[i] != '\0')
+		i++;
+	token = ft_substr(*line, 0, i);
+	*line = *line + i;
+	return (token);
+}
+
+char *treat_pipe(char **line)
+{
+	char *token;
+	int i;
+
+	i = 0;
+	while ((*line)[i] == '|' && (*line)[i] != '\0')
+		i++;
+	token = ft_substr(*line, 0, i);
+	*line = *line + i;
+	return (token);
+}
+
+char *treat_semicolon(char **line)
+{
+	char *token;
+	int i;
+
+	i = 0;
+	while ((*line)[i] == ';' && (*line)[i] != '\0')
+		i++;
+	token = ft_substr(*line, 0, i);
+	*line = *line + i;
+	return (token);
+}
+
+char *treat_simple_word(char **line)
+{
+	char *token;
+	int i;
+
+	i = 0;
+	while ((*line)[i] != ' ' && (*line)[i] != '\'' && (*line)[i] != '"'
+	&& (*line)[i] != '|' && (*line)[i] != ';' && (*line)[i] != '\0')
+		i++;
+	token = ft_substr(*line, 0, i);
+	*line = *line + i;
+	return (token);
+}
+
+char *get_token(char **line)
+{
+	char *token;
 
 	token = NULL;
 	if (**line == '"')
 		token = treat_double_quotes(line);
+	else if (**line == '\'')
+		token = treat_single_quotes(line);
+	else if (**line == '|')
+		token = treat_pipe(line);
+	else if (**line == ';')
+		token = treat_semicolon(line);
 	else if (**line != ' ')
 		token = treat_simple_word(line);
 	return (token);
 }
 
-
-void	print_tokens(t_tokens *tokens)
+void print_tokens(t_tokens *tokens)
 {
 	printf("MAH TOKENS BRO\n");
 	while (tokens)
@@ -361,11 +401,11 @@ void	print_tokens(t_tokens *tokens)
 	}
 }
 
-void	line_tokenizing(char *line, t_tokens **tokens_list)
+void line_tokenizing(char *line, t_tokens **tokens_list)
 {
-	char		*token;
-	t_type		type;
-	t_tokens	*curr_token;
+	char *token;
+	t_type type;
+	t_tokens *curr_token;
 
 	///
 	type = e_word;
@@ -373,30 +413,28 @@ void	line_tokenizing(char *line, t_tokens **tokens_list)
 	token = get_token(&line);
 	*tokens_list = create_single_token(token, type);
 	curr_token = *tokens_list;
-	
+
 	while (*line)
 	{
+		if (*line == ' ')
+			line++;
 		token = get_token(&line);
 		if (token != NULL)
 		{
 			curr_token->next = create_single_token(token, type);
 			curr_token = curr_token->next;
 		}
-		line++;
 	}
-
 
 	print_tokens(*tokens_list);
 }
 ///////////////////////////////////////////////////////////////////////////////////
 
-
-int		main(int argc, char **argv, char **env)
+int main(int argc, char **argv, char **env)
 {
-	char		*line;
-	t_node		*mother_node;
-	t_tokens	*tokens;
-
+	char *line;
+	t_node *mother_node;
+	t_tokens *tokens;
 
 	print_header();
 	while (1337)
@@ -404,15 +442,13 @@ int		main(int argc, char **argv, char **env)
 		print_current_dir();
 		get_next_line(0, &line);
 		printf("\ncmd line given : >%s<\n", line);
-		
+
 		line_tokenizing(line, &tokens);
 		// print_tokens(tokens);
 
 		//parse_line(&line, &mother_node);
 		print_preorder(mother_node);
-
 	}
-
 
 	// ////word///
 	// curr_word_node = get_bottom_node(curr_cmd_node);///
@@ -422,8 +458,7 @@ int		main(int argc, char **argv, char **env)
 	// curr_word_node = get_next_node(curr_word_node);///
 	// fill_word_node(curr_word_node, "-a");///
 	// ///////////
-	
-		
+
 	// curr_cmd_node = get_next_node(curr_cmd_node);
 	// fill_cmd_node(curr_cmd_node);
 
@@ -433,10 +468,10 @@ int		main(int argc, char **argv, char **env)
 	// curr_word_node = get_next_node(curr_word_node);///
 	// fill_word_node(curr_word_node, "10");///
 	// ///////////
-	
+
 	// curr_cmd_node = get_next_node(curr_cmd_node);
 	// fill_cmd_node(curr_cmd_node);
-	
+
 	// ///word///
 	// curr_word_node = get_bottom_node(curr_cmd_node);///
 	// fill_word_node(curr_word_node, "cat");///
@@ -446,7 +481,6 @@ int		main(int argc, char **argv, char **env)
 	// fill_word_node(curr_word_node, "file1");///
 	// ///////////
 
-
 	// print_preorder(mother_node);
 
 	// next_cmd_node = NULL;
@@ -454,22 +488,17 @@ int		main(int argc, char **argv, char **env)
 	//fill_cmd_node(curr_cmd_node, next_cmd_node);
 	// printf(">%d<\n", mother_node->bottom->next->tag);
 
-	
 	// t_node	**curr_cmd_node;
 	// curr_cmd_node = &(mother_node->bottom);
 	// *curr_cmd_node = create_simple_command(NULL, NULL);
 	// printf(">%d<\n", mother_node->bottom->tag);
-		
-	
 
-	
-	
 	// t_node *mother_node = create_pipline();
-	
+
 	// mother_node->bottom = create_simple_command(NULL, NULL);
 	// mother_node->bottom->next = create_simple_command(NULL, NULL);
 	// mother_node->bottom->next->next = create_simple_command(NULL, NULL);
-	
+
 	// mother_node->bottom->bottom = create_word("ls");
 	// mother_node->bottom->bottom->next = create_word("-l");
 	// mother_node->bottom->bottom->next->next = create_word("-a");
@@ -479,9 +508,7 @@ int		main(int argc, char **argv, char **env)
 	// mother_node->bottom->next->next->bottom = create_word("cat");
 	// mother_node->bottom->next->next->bottom->next = create_word("-e");
 
-	
-
-	// ///////execute cat command example//////////////	
+	// ///////execute cat command example//////////////
 	// char	**newargv = malloc(sizeof(char*) * 4);	//
 	// newargv[0] = strdup("/bin/cat");				//
 	// newargv[1] = strdup("main.c");				//
@@ -489,12 +516,6 @@ int		main(int argc, char **argv, char **env)
 	// newargv[3] = 0;								//
 	// execve("/bin/cat", newargv, env);			//
 	// ////////////////////////////////////////////////
-
-	
-
-
-
-	
 
 	return (0);
 }
