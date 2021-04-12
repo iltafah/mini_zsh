7754fc14c7f65541509f48cd21becd39b2532380
Ilias Tafah <iltafah@e2r4p3.1337.ma>
2021-04-12 17:11:14 +0000

    traversing ast has never been easier than it is right now

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 09:28:37 by iltafah           #+#    #+#             */
-/*   Updated: 2021/04/12 12:03:36 by iltafah          ###   ########.fr       */
+/*   Updated: 2021/04/12 17:00:08 by iltafah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -381,12 +381,101 @@ void print_header()
 	printf("╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝\n");
 }
 
+
+
+/*
+** ************************************************************************** **
+** ************************************************************************** **
+*/
+t_ast *get_curr_pipeline_seq_node(t_ast *ast)
+{
+	static t_ast	*curr_pipeline_seq = NULL;
+	static int		first = 1;
+
+	if (ast)
+	{
+		if (first == 1)
+			curr_pipeline_seq = ast->node.dir.bottom;
+		else if (curr_pipeline_seq != NULL)
+			curr_pipeline_seq = curr_pipeline_seq->node.dir.next;
+		first++;
+	}
+	return (curr_pipeline_seq);
+}
+
+t_ast *get_curr_smpl_cmd_node(t_ast *pipeline_seq)
+{
+	static t_ast	*curr_smpl_cmd = NULL;
+	static int		first = 1;
+
+	if (pipeline_seq)
+	{
+		if (first == 1)
+			curr_smpl_cmd = pipeline_seq->node.dir.bottom;
+		else if (curr_smpl_cmd != NULL)
+			curr_smpl_cmd = curr_smpl_cmd->node.dir.next;
+		first++;
+	}
+	if (curr_smpl_cmd == NULL)
+		first = 1;
+	return (curr_smpl_cmd);
+}
+
+void	print_args(t_ast *data_node)
+{
+	int		i;
+	int		len;
+	int		max_len;
+	char	**args;
+
+	i = 0;
+	len = 0;
+	max_len = 0;
+	args = data_node->node.data.args;
+	printf("%s[\n%s", PRP,WHT);
+	while (args[i])
+	{
+		len = printf("  %s\n", args[i++]);
+		if (max_len < len)
+			max_len = len;
+	}
+	printf("%*s",max_len, "");
+	printf("%s]\n", PRP);
+	printf("---------------------------------------------------------------\n\n");
+}
+
+void	execute_test(t_ast *ast)
+{
+	t_ast *curr_pipeline_seq;
+	t_ast *curr_simple_cmd;
+	t_ast *curr_data_node;
+
+	curr_pipeline_seq = get_curr_pipeline_seq_node(ast);
+	while (curr_pipeline_seq)
+	{	
+		curr_simple_cmd = get_curr_smpl_cmd_node(curr_pipeline_seq);
+		printf("%s ╔███████████████████████ Pipeline ███████████████████████╗\n\n",RED);
+		while (curr_simple_cmd)
+		{
+			curr_data_node = curr_simple_cmd->node.dir.bottom;
+			print_cmd_redirection(curr_data_node);
+			print_args(curr_data_node);
+			curr_simple_cmd = get_curr_smpl_cmd_node(curr_pipeline_seq);
+		}
+		curr_pipeline_seq = get_curr_pipeline_seq_node(ast);
+	}
+}
+/*
+** ************************************************************************** **
+** ************************************************************************** **
+*/
+
+
 int main(int argc, char **argv, char **env)
 {
 	char *line = NULL;
-	t_ast *ast = NULL;
 	t_tokens *tokens_list = NULL;
-	int		xd = 0;
+	t_ast *ast = NULL;
 
 	print_header();
 	while (1337)
@@ -404,8 +493,7 @@ int main(int argc, char **argv, char **env)
 		}
 		create_abstract_syntax_tree(&ast, tokens_list);
 		print_preorder(ast, 1);
-		if (strcmp(line, "exit") == 0)
-			xd = 1;
+		execute_test(ast);
 		/////////////////////////////////
 		/**		  freeing time		**///
 		/////////////////////////////////
@@ -413,8 +501,6 @@ int main(int argc, char **argv, char **env)
 		free_abstract_syntax_tree(ast);//
 		free(line);					   //
 		/////////////////////////////////
-		if (xd == 1)
-			exit(0);
 	}
 	return (0);
 }
--- parser/create_abstract_syntax_tree/storing_word_in_suitable_node.c
+++ parser/create_abstract_syntax_tree/storing_word_in_suitable_node.c
@@ -1,6 +1,6 @@
 #include "create_abstract_syntax_tree.h"
 
-void	get_curr_pipe_seq_node(t_state state, t_ast_ptrs *ast)
+void	get_suitable_pipe_seq_node(t_state state, t_ast_ptrs *ast)
 {
 	if (state.pipe_seq_node == NONE)
 		ast->curr_pipe_seq = create_pipe_seq_node(ast->cmd_line, BOTTOM);
@@ -8,7 +8,7 @@ void	get_curr_pipe_seq_node(t_state state, t_ast_ptrs *ast)
 		ast->curr_pipe_seq = create_pipe_seq_node(ast->curr_pipe_seq, NEXT);
 }
 
-void	get_curr_simple_cmd_node(t_state state, t_ast_ptrs *ast)
+void	get_suitable_simple_cmd_node(t_state state, t_ast_ptrs *ast)
 {
 	if (state.simple_command_node == NONE)
 		ast->curr_smpl_cmd = create_smpl_cmd_node(ast->curr_pipe_seq, BOTTOM);
@@ -16,7 +16,7 @@ void	get_curr_simple_cmd_node(t_state state, t_ast_ptrs *ast)
 		ast->curr_smpl_cmd = create_smpl_cmd_node(ast->curr_smpl_cmd, NEXT);
 }
 
-void	get_curr_data_node(t_state state, t_ast_ptrs *ast)
+void	get_suitable_data_node(t_state state, t_ast_ptrs *ast)
 {
 	if (state.data_node == NONE)
 		ast->curr_data = create_data_node(ast->curr_smpl_cmd, BOTTOM);
@@ -27,9 +27,9 @@ void	get_curr_data_node(t_state state, t_ast_ptrs *ast)
 
 void	store_word_in_suitable_node(t_ast_vars *vars, char *word)
 {
-	get_curr_pipe_seq_node(vars->state, &vars->ast_ptrs);
-	get_curr_simple_cmd_node(vars->state, &vars->ast_ptrs);
-	get_curr_data_node(vars->state, &vars->ast_ptrs);
+	get_suitable_pipe_seq_node(vars->state, &vars->ast_ptrs);
+	get_suitable_simple_cmd_node(vars->state, &vars->ast_ptrs);
+	get_suitable_data_node(vars->state, &vars->ast_ptrs);
 	vars->dynamic_table.ptr = &((*vars->ast_ptrs.curr_data)->node.data.args);
 	dynamic_table_join(&vars->dynamic_table, word);
 	(*vars->ast_ptrs.curr_data)->node.data.args = *vars->dynamic_table.ptr;
